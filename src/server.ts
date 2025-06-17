1c425d356a6782988ff2dc17fff83cc201c22df6
Marcus Soares <marcus.silva.soares1@gmail.com>
2025-06-17 12:01:54 -0300

    Setting routers in the file

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import dotenv from'dotenv'
 dotenv.config()
 import express from 'express'
 import cors from 'cors'
+import { errorHandler } from './middlewares/errorHandler'
+import { categoriesRouter } from './routes/categoriesRouter'
+import { rentalsRouter } from './routes/rentalRouter'
+import { usersRouter } from './routes/usersRouter'
+import { vehiclesRouter } from './routes/vehiclesRouter'
 
 const PORT = process.env.PORT ?? 3000
 const PROTOCOL = process.env.PROTOCOL ?? 'http'
@@ -15,5 +20,12 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Servidor da simulação de aluguel de carros rodando!');
 })
+app.use(categoriesRouter)
+app.use(rentalsRouter)
+app.use(usersRouter)
+app.use(vehiclesRouter)
+
+app.use(errorHandler)
+
 
 app.listen(PORT, ()=>console.log(`${PROTOCOL}://${BASE_URL}:${PORT}/`))
