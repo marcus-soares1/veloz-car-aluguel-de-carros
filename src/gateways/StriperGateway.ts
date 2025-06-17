72b1d37075464e6d6ba89dfba669b23dc0d80380
Marcus Soares <marcus.silva.soares1@gmail.com>
2025-06-17 11:54:26 -0300

    Creating gateway for striper

---
+++ src/gateways/StriperGateway.ts
@@ -0,0 +1,18 @@
+import { IPaymentGateway } from "./interface/IPaymentGateway";
+
+export class StriperGateway implements IPaymentGateway {
+    async processPayment() {
+
+    }
+
+    async createPayment() {
+    
+
+    }
+
+    async refundClient() {
+       
+
+    }
+
+}
